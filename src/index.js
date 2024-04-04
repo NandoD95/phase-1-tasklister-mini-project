96909fc5fad50db187d15ad24b0d6b150148f2a3
Fernando De Jesus <fdejesus95@outlook.com>
2024-04-04 16:00:52 -0400

    completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const newForm = document.querySelector('form')
+  const userInput = document.getElementById("new-task-description")
+  const newTasks = document.getElementById("tasks")
+
+  function addTask(){
+    const li = document.createElement('li')
+    li.textContent = userInput.value 
+    newTasks.appendChild(li)
+  }
+
+  newForm.addEventListener("submit", (e) => {
+    e.preventDefault()
+    addTask()
+  })
+
 });
